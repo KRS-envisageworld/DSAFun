6415b03861d1b17479654810edc4e7fc2545e6a8
KRS-envisageworld <83295488+KRS-envisageworld@users.noreply.github.com>
2023-09-27 17:09:40 +0530

    Update SlidingWindow-MinSubArraySum.js

--- SlidingWindow-MinSubArraySum.js
+++ SlidingWindow-MinSubArraySum.js
@@ -21,3 +21,7 @@ function minSubArrayLen(arr, target){
     }
     return returnValue;
 }
+
+minSubArrayLen([2,3,1,2,4,3], 7);
+minSubArrayLen([2,1,6,5,4], 9);
+minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52);
