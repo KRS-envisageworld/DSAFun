af090429e11d06505eacfb6e30784ed454094b19
KRS-envisageworld <83295488+KRS-envisageworld@users.noreply.github.com>
2023-09-27 17:08:15 +0530

    Update SlidingWindow-MaxSubArraySum.js

--- SlidingWindow-MaxSubArraySum.js
+++ SlidingWindow-MaxSubArraySum.js
@@ -1,16 +1,15 @@
-function maxSubarraySum(arr, num){
-  let maxSum = 0;
-  let tempSum = 0;
-  if (arr.length < num) return null;
-  for (let i = 0; i < num; i++) {
-    maxSum += arr[i];
-  }
-  tempSum = maxSum;
-  for (let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
-  }
-  return maxSum;
-}
+function maxSubarraySum(array, windowSize) {
+    if(array.length === 0 || array.length < windowSize) return null;
 
-maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+    let max= 0;
+    for(let i = 0 ;i<windowSize;i++){
+        max += array[i];
+    }
+    let value = max;
+    for(let i= windowSize; i<array.length; i++){
+        value =value - array[i-windowSize] + array[i];
+        max = Math.max(max,value);
+    }
+
+    return max;
+}
