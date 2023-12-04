2c5227e8b510e54d0dc71a1383078369a456e804
KRS-envisageworld <83295488+KRS-envisageworld@users.noreply.github.com>
2023-12-04 18:23:30 +0530

    Update SinglyLinkedList.js

--- Linked List/SinglyLinkedList.js
+++ Linked List/SinglyLinkedList.js
@@ -106,7 +106,57 @@ class SL{
         }
         this.currentList();
     }
-    
+      rotate(index){
+        if(this.length == 0 || index > this.length) return this;
+        let newHead,breakingNode;
+        let currentHead = this.head;
+        
+        let currentNode = currentHead;
+        let counter =1;
+        if(index < 0){
+            index = this.length + index;
+            if(this.length < 0) return this;
+        }
+        while(counter != index && currentNode.next != null){
+            currentNode = currentNode.next;    
+            counter++;
+        }
+        if(this.length == index){
+                newHead = currentNode;
+        }
+        else{
+            newHead = currentNode.next;
+        }
+        
+        currentNode.next = null;
+        
+        this.head = newHead;
+        while(newHead.next !=null){
+            newHead = newHead.next;
+        }
+        newHead.next = currentHead;
+        this.tail = currentNode;
+        return this;
+    }
+    set(index, val){
+        if(this.length == 0 || index > this.length-1 || index < 0) return false;
+        if(index == 0) {
+            this.head.val = val;
+        }
+        else if(index == this.length-1){
+            this.tail.val = val;
+        }
+        else{
+            let currentNode = this.head;
+            let counter = 0;
+            while(counter != index){
+                currentNode = currentNode.next;
+                counter++;
+            }
+            currentNode.val = val;
+        }
+        return true;
+    }
     currentList(){
         return this;
     }
